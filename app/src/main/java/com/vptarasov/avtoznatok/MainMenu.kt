ee4702ea459adaa4d39a75126f7f1122d48550a4
TarasovVP <vptarasov1998@gmail.com>
2019-08-02 16:16:27 +0300

    Initial commit

---
+++ app/src/main/java/com/vptarasov/avtoznatok/MainMenu.kt
@@ -0,0 +1,30 @@
+package com.vptarasov.avtoznatok
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainMenu : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.main_menu, container, false)
+    }
+
+
+
+}
